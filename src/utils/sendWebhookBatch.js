d76e29a449adf7326f0fb0bd65d8f21d5610ed2a
Renoks-Dev <renoks150@gmail.com>
2025-04-09 21:08:59 +0100

    Add retry logic when sending to Discord Webhook

--- src/utils/sendWebhookBatch.js
+++ src/utils/sendWebhookBatch.js
@@ -11,17 +11,16 @@ async function sendWebhookBatch(embeds, webhookUrl) {
       } embeds.`
     );
 
-    const response = await fetch(webhookUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ embeds: batch }),
-    });
-
-    if (!response.ok) {
+    const success = await sendBatchWithRetry(batch, webhookUrl);
+    if (!success) {
       console.error(
         `Failed to send Batch ${
           Math.floor(i / batchSize) + 1
-        } logs to Discord: ${response.statusText}`
+        } logs to Discord: ${
+          response && response.statusText
+            ? response.statusText
+            : "Unknown error"
+        } after ${maxRetries} retries.`
       );
     } else {
       console.log(
@@ -31,6 +30,51 @@ async function sendWebhookBatch(embeds, webhookUrl) {
       );
     }
   }
+}
+
+async function sendBatchWithRetry(batch, webhookUrl) {
+  const maxRetries = 5;
+  const retryDelay = 1000;
+  let attempt = 0;
+
+  while (attempt < maxRetries) {
+    try {
+      const response = await fetch(webhookUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ embeds: batch }),
+      });
+
+      if (!response.ok) {
+        console.log(response);
+        console.error(
+          `Failed to send logs to the Discord Webhook. Status: ${response.statusText}`
+        );
+        attempt++;
+
+        const delay = retryDelay * attempt;
+        console.log(`Retrying in ${delay / 1000} seconds...`);
+
+        await delayFunction(delay);
+      } else {
+        return true;
+      }
+    } catch (error) {
+      console.error(`Error sending logs: ${error.message}`);
+      attempt++;
+
+      const delay = retryDelay * attempt;
+
+      console.log(`Retrying in ${delay / 1000} seconds...`);
+      await delayFunction(delay);
+    }
+  }
+
+  return false;
+}
+
+function delayFunction(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 module.exports = { sendWebhookBatch };
